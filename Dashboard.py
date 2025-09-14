96be7b57db5d30025931981e1c09336e934c779b
Wiv123 <karingrandin@live.se>
2025-09-14 16:00:15 +0200

    add dataset and title text

--- Dashboard.py
+++ Dashboard.py
@@ -1,66 +1,24 @@
 import streamlit as st
-
-st.set_page_config(
-    page_title="PROHI Dashboard",
-    page_icon="👋",
-)
-import pandas as pd
-from numpy.random import default_rng as rng
-
-# Sidebar configuration
-st.sidebar.image("./assets/project-logo.jpg",)
-st.sidebar.success("Select a tab above.")
-
-# # Page information
-
-st.write("# Welcome to PROHI Dashboard! 👋")
-
-st.markdown(
-"""
-    ## Aims
-
-    After completing the course the student should be able to:
-    - explain basic project management methods
-    - be able to account for success factors in Health Informatics projects
-    - understand basic methods and tools in the field of data science and machine learning
-    - explain process models for data mining projects
-    - explain the difference between rule-based methods and machine learning methods
-    - apply basic project management methods
-    - work in an international multidisciplinary project group
-    - independently lead and implement a limited project in health informatics - document the steps in the design of a prototype for a health informatics project
-    - apply the steps in a process model for data mining projects
-    - apply methods from the field of text mining on different types of health informatics problems
-    - explain and argue for their positions regarding the implementation of a health informatics project
-    - explain how to work with sensitive health information in a safe and ethical way.
-
-"""
-)
-
-# You can also add text right into the web as long comments (""")
-"""
-The final project aims to apply data science concepts and skills on a 
-medical case study that you and your team select from a public data source.
-The project assumes that you bring the technical Python skills from 
-previous courses (*DSHI*: Data Science for Health Informatics), as well as 
-the analytical skills to argue how and why specific techniques could
-enhance the problem domain related to the selected dataset.
-"""
-
-### UNCOMMENT THE CODE BELOW TO SEE EXAMPLE OF INPUT WIDGETS
-
-# DATAFRAME MANAGEMENT
 import numpy as np
+import pandas as pd
+from faker import Faker
+import random
+import faker
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
 
-# Add a slider to the sidebar:
-add_slider = st.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+df = pd.read_csv("speech_pathology_dataset1.csv")
+df['Visit_Type'] = df['Visit Type'].replace('visit', 'Assistive technology evaluation')
 
-#Added area chart with 
-df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+import random
 
-st.area_chart(df)
+months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+df['Month of visit'] = [random.choice(months) for _ in range(len(df))]
+df['Year'] = [random.randint(2015, 2025) for _ in range(len(df))]
+
+st.title(":blue[LOGOPEDDATA SVERIGE]")
+st.markdown('''
+    :blue-background[_Swedish Speech and Language Pathology data for the years 2015-2025_]''')
