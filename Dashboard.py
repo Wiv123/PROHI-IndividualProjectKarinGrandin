c68c10a92585004441a157e1f866b1afe5438d6b
Wiv123 <karingrandin@live.se>
2025-09-14 17:29:30 +0200

    add picture

--- Dashboard.py
+++ Dashboard.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 from faker import Faker
 import random
-import faker
-
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
 
 df = pd.read_csv("speech_pathology_dataset1.csv")
 df['Visit_Type'] = df['Visit Type'].replace('visit', 'Assistive technology evaluation')
@@ -19,6 +20,13 @@ months = [
 df['Month of visit'] = [random.choice(months) for _ in range(len(df))]
 df['Year'] = [random.randint(2015, 2025) for _ in range(len(df))]
 
-st.title(":blue[LOGOPEDDATA SVERIGE]")
+st.title(":blue[LOGOPEDDATA Sverige]")
 st.markdown('''
     :blue-background[_Swedish Speech and Language Pathology data for the years 2015-2025_]''')
+
+image = Image.open('logopedbild.jpg') 
+
+st.sidebar.image(image, use_container_width=True)
+st.page_link("Dashboard.py", label="Page 1", icon="1️⃣")
+
+st.snow()
